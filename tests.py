56f8090af548f74a94856c70e3f4f2209aba438b
Adan Rosales <adan.rosales_c@janrain.com>
2019-09-29 23:09:49 -0500

    adding interested UT

--- tests.py
+++ tests.py
@@ -22,3 +22,10 @@ class Test_lcm(TestCase):
 
     def test_A003418_method_1_20(self):
         self.assertEqual(Least_common_multiple_A003418(1, 20), 232792560)
+    
+    def test_validating_A003418_first_solutions_no_reminder(self):
+        for n in range(4,20,2):
+            #make sure there is not numbers 
+            # # than reminder division is different 0 in the given range
+            result = Least_common_multiple_A003418(1, n)
+            self.assertEquals(int(len([ x for x in range (1, n) if result % x != 0 ])),0)
